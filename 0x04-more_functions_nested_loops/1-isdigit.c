91457a72157bb0cf19295b28241fafc376012d85
Zeugen23 <ulrichparvel@gmail.com>
2023-09-15 05:55:26 -0700

    all tasks

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: to be checked
+ * Return: returns something
+ */
+int _isdigit(int c)
+{
+	return(c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply asdasd
+ * @a: - inasdasdsad adfsaf
+ * @b: - sdfsdfsklnl asdasasdas
+ * Return: the results
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
