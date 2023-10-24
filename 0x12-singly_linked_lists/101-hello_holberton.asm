1adec8fffc94b55ec5a6a639407abd096f119d62
Zeugen23 <ulrichparvel@gmail.com>
2023-10-24 11:55:53 -0700

    n

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,12 @@
+
+format: db `Hello, Holberton\n`,0
+
+main:
+          mov   edi, format
+          xor   eax, eax
+          call  printf
+          mov   eax, 0
+          ret
+
+global    main
+          extern    printf
