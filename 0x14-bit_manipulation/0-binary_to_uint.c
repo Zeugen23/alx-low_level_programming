5128f8d6f4a6c235266a9bed2381ffe2aef12bf4
Zeugen23 <ulrichparvel@gmail.com>
2023-10-27 15:04:45 -0700

    FR

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,20 +10,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int a;
-        unsigned int num;
+	unsigned int num;
 
 	num = 0;
 	if (!b)
 		return (0);
 	for (a = 0; b[a] != '\0'; a++)
 	{
-		if (!(b[a] == '0' || b[a] == '1'))
+		if (b[a] != '0' && b[a] != '1')
 			return (0);
 	}
 	for (a = 0; b[a] != '\0'; a++)
 	{
-		num *= 2;
-		if (b[a] == '1')
+		num <<= 1;
+		if (*(b + a) == '1')
 			num += 1;
 	}
 	return (num);
