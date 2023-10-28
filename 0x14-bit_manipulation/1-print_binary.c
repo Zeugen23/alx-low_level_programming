3e5410310395f7e5d63422c9c4255b6377838958
Zeugen23 <ulrichparvel@gmail.com>
2023-10-27 22:42:25 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,7 +15,7 @@ unsigned long int _pow(unsigned int base, unsigned int power)
 
 	num = 1;
 	for (a = 1; a <= power; a++)
-		num = num * base;
+		num *= base;
 	return (num);
 }
 
@@ -35,7 +35,7 @@ void print_binary(unsigned long int n)
 	while (divisor != 0)
 	{
 		check = n & divisor;
-		if (!(check ^ divisor))
+		if (check == divisor)
 		{
 			flag = 1;
 			_putchar('1');
@@ -44,7 +44,7 @@ void print_binary(unsigned long int n)
 		{
 			_putchar('0');
 		}
-		divisor /= 2;
+		divisor >>= 1;
 	}
 }
 
