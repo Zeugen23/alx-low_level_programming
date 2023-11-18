71f9f93da12bcfd93cfedaeb81fa9000a2a7f273
Zeugen23 <ulrichparvel@gmail.com>
2023-11-18 10:30:55 -0800

    d

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,26 +2,26 @@
 #include "main.h"
 
 /**
- * flip_bits - counts the number of bits to change
- * to get from one number to another
- * @n: first number
- * @m: second number
+ * flip_bits - Counts the number of bits to change
+ *             to get from one number to another.
+ * @n: First number.
+ * @m: Second number.
  *
- * Return: number of bits to change
+ * Return: Number of bits to change.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int a, countbit = 0;
+	int a;
+	unsigned int count_bit = 0;
 	unsigned long int current;
 	unsigned long int exclusive = n ^ m;
 
-                for (a = 63; a >= 0; a--)
+	for (a = 63; a >= 0; a--)
 	{
 		current = exclusive >> a;
 		if ((current & 1) != 0)
-			countbit++;
+			count_bit++;
 	}
 
-	return (countbit);
+	return (count_bit);
 }
-
